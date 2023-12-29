5c9f4bcaa008f2362cdd6b311f92b31074bdf67c
M Rizky Ramadhani <69834119+Mokyra18@users.noreply.github.com>
2023-12-29 11:19:56 +0700

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -1,12 +1,12 @@
 import List from "@/components/list"
 
 export default function Home() {
-  // return (
-  //   <>
-  //     <List />
+  return (
+    <>
+      <List />
 
-  //   </>
-  // )
-  return <div>menu</div>
+    </>
+  )
+  // return <div>menu</div>
 
 }
